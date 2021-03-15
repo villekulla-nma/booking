16546a8c735613b66d935601002ad31c7a350413
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-15 21:07:05 +0100

    feat: return user object from verify-session endpoint

--- server/src/handlers/post-verify-session.ts
+++ server/src/handlers/post-verify-session.ts
@@ -1,6 +1,38 @@
+import type { RouteShorthandOptions } from 'fastify';
 import jwt from 'jsonwebtoken';
+import type { User } from '@villekulla-reservations/types';
 
 import type { AssignHandlerFunction } from './type';
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          user: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              firstName: { type: 'string' },
+              lastName: { type: 'string' },
+              email: { type: 'string' },
+            },
+          },
+          status: { type: 'string' },
+        },
+        required: ['user', 'status'],
+      },
+      '4xx': {
+        type: 'object',
+        properties: {
+          status: { type: 'string' },
+        },
+        required: ['status'],
+      },
+    },
+  },
+};
 
 const verifyToken = async (token: string): Promise<{ id?: string }> =>
   new Promise((resolve, reject) => {
@@ -15,26 +47,35 @@ const verifyToken = async (token: string): Promise<{ id?: string }> =>
 
 export const assignPostVerifySessionHandler: AssignHandlerFunction = (
   route,
-  server
+  server,
+  model
 ) => {
-  server.post(route, async (request, reply) => {
-    let status = 201;
+  server.post(route, opts, async (request, reply) => {
+    let status = 200;
+    const response: { status: string; user?: User } = { status: 'ok' };
     const [, token] =
       request.headers?.cookie?.match(/login=([^;]+)(;|$)/) || [];
 
     do {
       if (!token) {
+        response.status = 'error';
         status = 401;
         break;
       }
       const { id: userId } = await verifyToken(token);
 
       if (!userId) {
+        response.status = 'invalid';
         status = 400;
         break;
       }
+
+      const { id, firstName, lastName, email } = await model.getUserById(
+        userId
+      );
+      response.user = { id, firstName, lastName, email };
     } while (false);
 
-    reply.status(status).send();
+    reply.status(status).send(response);
   });
 };
