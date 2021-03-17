9e91014e125951ac9c5fc75330684cf256f6f3c8
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-17 20:29:05 +0100

    fix: provide explicit invalid result from user verification

--- client/src/hooks/use-session-checker.ts
+++ client/src/hooks/use-session-checker.ts
@@ -3,11 +3,11 @@ import type { User } from '@villekulla-reservations/types';
 
 import { verifySession } from '../api';
 
-export const useSessionChecker = (): User | undefined => {
-  const [user, setUser] = useState<User>();
+export const useSessionChecker = (): User | null | undefined => {
+  const [user, setUser] = useState<User | null>();
 
   useEffect(() => {
-    verifySession().then((result) => setUser(result));
+    verifySession().then((result) => setUser(result || null));
   }, []);
 
   return user;
