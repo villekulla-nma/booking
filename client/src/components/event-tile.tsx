d03a176021244ed0b56229e432b302e2ae514932
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-22 21:06:30 +0100

    feat: style the start page

---
+++ client/src/components/event-tile.tsx
@@ -0,0 +1,92 @@
+import type { FC } from 'react';
+import type { IStackTokens } from '@fluentui/react';
+import { Stack, Text, NeutralColors } from '@fluentui/react';
+import { mergeStyles } from '@fluentui/merge-styles';
+import { SharedColors, FluentTheme } from '@fluentui/theme';
+import { Link, useHistory } from 'react-router-dom';
+import classNames from 'classnames';
+import { format } from 'date-fns';
+import { de } from 'date-fns/locale';
+
+import { UserEvent } from '../api';
+
+interface Props {
+  className?: string;
+}
+
+const wrap = mergeStyles({
+  padding: '12px',
+  marginBottom: '16px',
+  backgroundColor: FluentTheme.palette.themeLighterAlt,
+  cursor: 'pointer',
+  ':hover': {
+    backgroundColor: NeutralColors.gray20,
+  },
+});
+
+const wrapTokens: IStackTokens = {
+  childrenGap: '8px',
+};
+
+const link = mergeStyles({
+  color: SharedColors.cyanBlue20,
+  textDecoration: 'none',
+});
+
+const listItemTokens: IStackTokens = {
+  padding: '4px 0',
+};
+
+const definitionTerm = mergeStyles({
+  flex: '0 1 15%',
+  color: NeutralColors.gray140,
+});
+
+const descriptionText = mergeStyles({
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
+
+const formatDate = (date: string) =>
+  format(new Date(date), 'd. MMMM y, H:mm', { locale: de });
+
+export const EventTile: FC<UserEvent & Props> = ({
+  id,
+  resource,
+  start,
+  end,
+  description,
+  className,
+}) => {
+  const history = useHistory();
+  const eventUrl = `/events/${id}`;
+  const handleClick = () => history.push(eventUrl);
+
+  return (
+    <Stack
+      tokens={wrapTokens}
+      className={classNames(wrap, className)}
+      onClick={handleClick}
+    >
+      <Text variant="large">
+        <Link to={eventUrl} className={link}>
+          {resource}
+        </Link>
+      </Text>
+      <dl>
+        <Stack horizontal={true} tokens={listItemTokens}>
+          <dt className={definitionTerm}>Start</dt>
+          <dd>{formatDate(start)}</dd>
+        </Stack>
+        <Stack horizontal={true} tokens={listItemTokens}>
+          <dt className={definitionTerm}>Ende</dt>
+          <dd>{formatDate(end)}</dd>
+        </Stack>
+      </dl>
+      <Text variant="medium" className={descriptionText}>
+        {description || '—'}
+      </Text>
+    </Stack>
+  );
+};
--- client/src/pages/start-page.tsx
+++ client/src/pages/start-page.tsx
@@ -1,33 +1,52 @@
 import type { FC } from 'react';
+import { Stack, Text } from '@fluentui/react';
+import { mergeStyles } from '@fluentui/merge-styles';
 
 import { Layout } from '../components/layout';
 import { useUserEvents } from '../hooks/use-user-events';
+import { EventTile } from '../components/event-tile';
+
+const heading = mergeStyles({
+  marginBottom: '32px',
+});
+
+const text = mergeStyles({
+  fontStyle: 'italic',
+});
+
+const tile = mergeStyles({
+  width: 'calc(50% - 32px)',
+});
 
 export const StartPage: FC = () => {
-  const upcomingEvents = useUserEvents(3);
+  const upcomingEvents = useUserEvents(10);
 
   return (
     <Layout>
+      <Text variant="xLarge" className={heading}>
+        Anstehende Buchungen
+      </Text>
       {(() => {
         switch (true) {
           case typeof upcomingEvents === 'undefined':
-            return <p>Lade Buchungen&hellip;</p>;
+            return (
+              <Text variant="medium" className={text}>
+                Lade Buchungen&hellip;
+              </Text>
+            );
           case upcomingEvents?.length === 0:
-            return <p>Es stehen keine Buchungen an.</p>;
+            return (
+              <Text variant="medium" className={text}>
+                Es stehen keine Buchungen an.
+              </Text>
+            );
           default:
-            return upcomingEvents?.map(
-              ({ id, start, end, description, resource }) => (
-                <dl key={id}>
-                  <dt>Ressource</dt>
-                  <dd>{resource}</dd>
-                  <dt>Start</dt>
-                  <dd>{start}</dd>
-                  <dt>Ende</dt>
-                  <dd>{end}</dd>
-                  <dt>Beschreibung</dt>
-                  <dd>{description || '—'}</dd>
-                </dl>
-              )
+            return (
+              <Stack horizontal={true} wrap={true} gap={32}>
+                {upcomingEvents?.map((event) => (
+                  <EventTile className={tile} key={event.id} {...event} />
+                ))}
+              </Stack>
             );
         }
       })()}
