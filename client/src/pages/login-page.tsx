23e367e4f6c43db66d213f6b1d09b15eaf1cb3f2
herschel666 <kraemerseele@gmx.de>
2021-03-23 07:14:17 +0100

    refactor: use generic form component

--- client/src/pages/login-page.tsx
+++ client/src/pages/login-page.tsx
@@ -1,34 +1,11 @@
-import type { FC, SyntheticEvent, FormEvent } from 'react';
+import type { FC, FormEvent } from 'react';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import {
-  Link as A,
-  TextField,
-  PrimaryButton,
-  Stack,
-  Text,
-  NeutralColors,
-} from '@fluentui/react';
-import type { IStackTokens } from '@fluentui/react';
-import { mergeStyles } from '@fluentui/merge-styles';
+import { Link as A, TextField } from '@fluentui/react';
 
 import { login } from '../api';
 import { Layout } from '../components/layout';
-
-const form = mergeStyles({
-  width: '100%',
-  maxWidth: '500px',
-  margin: '0 auto',
-});
-
-const fieldset = mergeStyles({
-  marginBottom: '16px',
-  border: `1px solid ${NeutralColors.gray90}`,
-});
-
-const fieldsetTokens: IStackTokens = {
-  childrenGap: 12,
-};
+import { Form } from '../components/form';
 
 export const LoginPage: FC = () => {
   const location = useLocation<{ from: string }>();
@@ -47,9 +24,7 @@ export const LoginPage: FC = () => {
   ): void => {
     setPassword(event.currentTarget.value);
   };
-  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-
+  const handleSubmit = (): void => {
     login(email.trim(), password.trim()).then((result) => {
       switch (result) {
         case 'ok':
@@ -74,47 +49,41 @@ export const LoginPage: FC = () => {
 
   return (
     <Layout>
-      <form className={form} method="post" onSubmit={handleSubmit}>
-        <Stack as="fieldset" className={fieldset} tokens={fieldsetTokens}>
-          <Text as="legend" variant="large">
-            Anmeldung
-          </Text>
-          {feedback && (
-            <div>
-              <b>{feedback}</b>
-            </div>
-          )}
+      <Form label="Anmeldung" onSubmit={handleSubmit}>
+        {feedback && (
           <div>
-            <TextField
-              label="Email"
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              placeholder="Dein Email-Adresse &hellip;"
-              onChange={handleEmailChange}
-            />
+            <b>{feedback}</b>
           </div>
-          <div>
-            <TextField
-              label="Passwort"
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              placeholder="Dein Passwort &hellip;"
-              canRevealPassword={true}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <p>
-            <A as={Link} to="/password-reset">
-              Passwort vergessen?
-            </A>
-          </p>
-        </Stack>
-        <PrimaryButton>Absenden</PrimaryButton>
-      </form>
+        )}
+        <div>
+          <TextField
+            label="Email"
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            placeholder="Dein Email-Adresse &hellip;"
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <TextField
+            label="Passwort"
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            placeholder="Dein Passwort &hellip;"
+            canRevealPassword={true}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <p>
+          <A as={Link} to="/password-reset">
+            Passwort vergessen?
+          </A>
+        </p>
+      </Form>
     </Layout>
   );
 };
