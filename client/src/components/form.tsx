3b4fd16acf29aadb0b8459b82b8fef2dad65b22c
herschel666 <kraemerseele@gmx.de>
2021-03-23 07:14:03 +0100

    feat: add generic form component

---
+++ client/src/components/form.tsx
@@ -0,0 +1,43 @@
+import type { FC, FormEvent } from 'react';
+import { Stack, Text, PrimaryButton, NeutralColors } from '@fluentui/react';
+import type { IStackTokens } from '@fluentui/react';
+import { mergeStyles } from '@fluentui/merge-styles';
+
+interface Props {
+  onSubmit: () => void;
+  label: string;
+}
+
+const form = mergeStyles({
+  width: '100%',
+  maxWidth: '500px',
+  margin: '0 auto',
+});
+
+const fieldset = mergeStyles({
+  marginBottom: '16px',
+  border: `1px solid ${NeutralColors.gray90}`,
+});
+
+const fieldsetTokens: IStackTokens = {
+  childrenGap: 12,
+};
+
+export const Form: FC<Props> = ({ children, onSubmit, label }) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <form method="post" onSubmit={handleSubmit} className={form}>
+      <Stack as="fieldset" className={fieldset} tokens={fieldsetTokens}>
+        <Text as="legend" variant="large">
+          {label}
+        </Text>
+        {children}
+      </Stack>
+      <PrimaryButton>Absenden</PrimaryButton>
+    </form>
+  );
+};
