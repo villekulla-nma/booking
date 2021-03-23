59644c1e7ec074a1aea97e0f896954d87cdbd647
herschel666 <kraemerseele@gmx.de>
2021-03-23 06:50:45 +0100

    fix: check user state before doing API calls

--- client/src/hooks/use-resource-list.ts
+++ client/src/hooks/use-resource-list.ts
@@ -2,13 +2,17 @@ import { useState, useEffect } from 'react';
 import type { ResourceAttributes } from '@villekulla-reservations/types';
 
 import { getResources } from '../api';
+import { useUser } from './use-user';
 
 export const useResourceList = (): ResourceAttributes[] => {
+  const user = useUser();
   const [list, setList] = useState<ResourceAttributes[]>([]);
 
   useEffect(() => {
-    getResources().then((resourceList) => setList(resourceList));
-  }, []);
+    if (user) {
+      getResources().then((resourceList) => setList(resourceList));
+    }
+  }, [user]);
 
   return list;
 };
--- client/src/hooks/use-user-events.ts
+++ client/src/hooks/use-user-events.ts
@@ -2,13 +2,17 @@ import { useEffect, useState } from 'react';
 
 import type { UserEvent } from '../api';
 import { getUserEvents } from '../api';
+import { useUser } from './use-user';
 
 export const useUserEvents = (limit?: number): UserEvent[] | undefined => {
+  const user = useUser();
   const [events, setEvents] = useState<UserEvent[] | undefined>();
 
   useEffect(() => {
-    getUserEvents(limit).then((events) => setEvents(events));
-  }, [limit]);
+    if (user) {
+      getUserEvents(limit).then((events) => setEvents(events));
+    }
+  }, [limit, user]);
 
   return events;
 };
