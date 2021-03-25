3391bf3f3cfbc0f06b49c9219d9091f484204521
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-25 15:59:41 +0100

    fix: remove mailing feature

--- server/src/handlers/post-password-reset.ts
+++ server/src/handlers/post-password-reset.ts
@@ -1,8 +1,6 @@
 import type { RouteShorthandOptions } from 'fastify';
 
 import type { AssignHandlerFunction } from './type';
-import { sendMail } from '../utils/send-mail';
-import { env } from '../utils/env';
 import { randomBytes } from '../utils/crypto';
 import { getUserByKey, updateUser } from '../controllers/user';
 
@@ -38,23 +36,9 @@ export const assignPostPasswordResetHandler: AssignHandlerFunction = (
 
       const token = randomBytes(32);
 
-      server.log.debug('Setting reset token %s for user %s', token, user.id);
+      server.log.warn('Setting reset token %s for user %s', token, user.id);
 
       await updateUser(db, user.id, { passwordReset: token });
-
-      const link = `${env('APP_URL')}/password-reset/${token}`;
-      const subject = 'Passwort-Reset';
-      const text = `Moin ${user.firstName}.
-
-Du hast einen Passwort-Reset angefordert. Klicke diesen Link um fortzufahren:
-
-${link}
-
-Solltest du den Passwort-Reset nicht angefordert haben, kannst du diese Email ignorieren.
-
-MfG`;
-
-      await sendMail(user.email, subject, text);
     } while (false);
 
     reply.send({ status: 'ok' });
