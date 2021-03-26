92b05f369de1eb838252eef375b2b64391737a41
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-26 16:21:02 +0100

    chore(server): remove superfluous log call

--- server/src/db.ts
+++ server/src/db.ts
@@ -51,7 +51,6 @@ export const initDb = async (): Promise<Db> => {
 
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
     throw error;
