7849f948bed7ccbdb3e057605e573bf428b39355
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-27 11:36:00 +0100

    test(server): add tests for endpoint /api/resources/:resourceId/events

---
+++ server/src/__tests__/server-put-event.ts
@@ -0,0 +1,207 @@
+import type { FastifyInstance } from 'fastify';
+import fetch from 'node-fetch';
+import { Op } from 'sequelize';
+
+import type { Db } from '../db';
+import { initDb } from '../db';
+import { initServer } from '../server';
+import { signJwt } from './helpers/sign-jwt';
+import type { ResourceInstance } from '../models/resource';
+import { createEvent, getOverlappingEvents } from '../controllers/event';
+
+jest.mock('../controllers/event');
+
+describe('Server [PUT] /api/resources/:resourceId/events', () => {
+  const [tomorrow] = new Date(Date.now() + 24 * 3600 * 1000)
+    .toISOString()
+    .split('T');
+  let cookieValue: string;
+  let server: FastifyInstance;
+  let db: Db;
+  let log: Console['log'];
+
+  beforeAll(async () => {
+    db = await initDb();
+    server = await initServer(db, '9060');
+    log = console.log;
+
+    console.log = () => undefined;
+
+    await db.User.create({
+      id: 'TD0sIeaoz',
+      email: 'person.one@example.com',
+      firstName: 'Person1',
+      lastName: 'One',
+      role: 'user',
+      groupId: 'YLBqxvCCm',
+    });
+    await db.User.create({
+      id: 'Ul2Zrv1BX',
+      email: 'person.two@example.com',
+      firstName: 'Person2',
+      lastName: 'Two',
+      role: 'user',
+      groupId: 'MTpZEtFhN',
+    });
+    await db.Resource.bulkCreate<ResourceInstance>([
+      {
+        id: 'Uj5SAS740',
+        name: 'Resource #1',
+      },
+      {
+        id: 'gWH5T7Kdz',
+        name: 'Resource #2',
+      },
+    ]);
+    await db.Event.create({
+      id: 'HBv7p7CVC',
+      start: `${tomorrow}T08:30:00.000Z`,
+      end: `${tomorrow}T12:00:00.000Z`,
+      allDay: false,
+      resourceId: 'Uj5SAS740',
+      description: 'A nice event',
+      userId: 'Ul2Zrv1BX',
+    });
+
+    cookieValue = await signJwt({ id: 'TD0sIeaoz' }, process.env.JWT_SECRET);
+  });
+
+  afterAll(async () => {
+    await db?.terminate();
+    server.close();
+
+    console.log = log;
+  });
+
+  it.each([
+    [1, `${tomorrow}T07:30:00.000Z`, `${tomorrow}T09:00:00.000Z`, false],
+    [2, `${tomorrow}T09:00:00.000Z`, `${tomorrow}T11:00:00.000Z`, false],
+    [3, `${tomorrow}T11:30:00.000Z`, `${tomorrow}T13:00:00.000Z`, false],
+    [4, `${tomorrow}T00:00:00.000Z`, `${tomorrow}T23:59:59.000Z`, true],
+  ])(
+    'should respond with 400/overlapping [%i]',
+    async (_, start, end, allDay) => {
+      (getOverlappingEvents as jest.Mock).mockImplementation(
+        jest.requireActual('../controllers/event').getOverlappingEvents
+      );
+
+      const response = await fetch(
+        'http://localhost:9060/api/resources/Uj5SAS740/events',
+        {
+          method: 'PUT',
+          headers: {
+            cookie: `login=${cookieValue}`,
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            description: '',
+            start,
+            end,
+            allDay,
+          }),
+        }
+      );
+      const data = await response.json();
+
+      expect(response.status).toBe(400);
+      expect(data.status).toBe('overlapping');
+    }
+  );
+
+  it('should respond with 400/invalid on Sequelize Validation Error', async () => {
+    (getOverlappingEvents as jest.Mock).mockImplementation(
+      jest.requireActual('../controllers/event').getOverlappingEvents
+    );
+    (createEvent as jest.Mock).mockRejectedValue(
+      Object.assign(new Error('wrong'), { name: 'SequelizeValidationError' })
+    );
+
+    const response = await fetch(
+      'http://localhost:9060/api/resources/Uj5SAS740/events',
+      {
+        method: 'PUT',
+        headers: {
+          cookie: `login=${cookieValue}`,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          description: '',
+          start: '1234-56-78T11:22:33.000Z',
+          end: '1234-56-78T12:22:33.000Z',
+          allDay: false,
+        }),
+      }
+    );
+    const data = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(data.status).toBe('invalid');
+  });
+
+  it('should respond with 500/error on failure', async () => {
+    (getOverlappingEvents as jest.Mock).mockImplementation(
+      jest.requireActual('../controllers/event').getOverlappingEvents
+    );
+    (createEvent as jest.Mock).mockRejectedValue(new Error('nope'));
+
+    const response = await fetch(
+      'http://localhost:9060/api/resources/Uj5SAS740/events',
+      {
+        method: 'PUT',
+        headers: {
+          cookie: `login=${cookieValue}`,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          description: '',
+          start: '1234-56-78T11:22:33.000Z',
+          end: '1234-56-78T12:22:33.000Z',
+          allDay: false,
+        }),
+      }
+    );
+    const data = await response.json();
+
+    expect(response.status).toBe(500);
+    expect(data.status).toBe('error');
+  });
+
+  it('should respond with 200/ok on success', async () => {
+    (getOverlappingEvents as jest.Mock).mockImplementation(
+      jest.requireActual('../controllers/event').getOverlappingEvents
+    );
+    (createEvent as jest.Mock).mockImplementation(
+      jest.requireActual('../controllers/event').createEvent
+    );
+
+    const start = `${tomorrow}T15:00:00.000Z`;
+    const end = `${tomorrow}T16:00:00.000Z`;
+    const response = await fetch(
+      'http://localhost:9060/api/resources/Uj5SAS740/events',
+      {
+        method: 'PUT',
+        headers: {
+          cookie: `login=${cookieValue}`,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          description: 'Fancy get-together',
+          allDay: false,
+          start,
+          end,
+        }),
+      }
+    );
+    const data = await response.json();
+    const event = await db.Event.findOne({
+      where: {
+        start: { [Op.eq]: start },
+        end: { [Op.eq]: end },
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(data.status).toBe('ok');
+    expect(event.description).toBe('Fancy get-together');
+  });
+});
