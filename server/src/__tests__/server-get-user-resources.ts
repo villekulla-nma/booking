074dfde1a96bf21beb147b937d96012ce3261ed7
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-27 10:39:41 +0100

    test(server): add tests for endpoint /api/resources

---
+++ server/src/__tests__/server-get-user-resources.ts
@@ -0,0 +1,64 @@
+import type { FastifyInstance } from 'fastify';
+import fetch from 'node-fetch';
+
+import type { Db } from '../db';
+import { initDb } from '../db';
+import { initServer } from '../server';
+import { signJwt } from './helpers/sign-jwt';
+import type { ResourceInstance } from '../models/resource';
+
+describe('Server [GET] /api/resources', () => {
+  let cookieValue: string;
+  let server: FastifyInstance;
+  let db: Db;
+
+  beforeAll(async () => {
+    db = await initDb();
+    server = await initServer(db, '9060');
+    await db.User.create({
+      id: 'TD0sIeaoz',
+      email: 'person.one@example.com',
+      firstName: 'Person1',
+      lastName: 'One',
+      role: 'user',
+      groupId: 'YLBqxvCCm',
+    });
+    await db.Resource.bulkCreate<ResourceInstance>([
+      {
+        id: 'Uj5SAS740',
+        name: 'Resource #1',
+      },
+      {
+        id: 'gWH5T7Kdz',
+        name: 'Resource #2',
+      },
+    ]);
+    cookieValue = await signJwt({ id: 'TD0sIeaoz' }, process.env.JWT_SECRET);
+  });
+
+  afterAll(async () => {
+    await db?.terminate();
+    server.close();
+  });
+
+  it('should respond with the resources', async () => {
+    const response = await fetch('http://localhost:9060/api/resources', {
+      headers: {
+        cookie: `login=${cookieValue}`,
+      },
+    });
+    const events = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(events).toEqual([
+      {
+        id: 'Uj5SAS740',
+        name: 'Resource #1',
+      },
+      {
+        id: 'gWH5T7Kdz',
+        name: 'Resource #2',
+      },
+    ]);
+  });
+});
