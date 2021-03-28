c6a679281c5e10e313a46060f65bdcab96e16669
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-28 16:48:28 +0200

    test(client): add check for password reset page

---
+++ client/src/pages/__tests__/password-reset-page.test.tsx
@@ -0,0 +1,49 @@
+import nock from 'nock';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter as Router, Route } from 'react-router-dom';
+import { initializeIcons } from '@uifabric/icons';
+
+import { PasswordResetPage } from '../password-reset-page';
+
+jest.mock('../../components/layout.tsx', () => ({
+  Layout: ({ children }) => <>{children}</>,
+}));
+
+describe('Resource Page', () => {
+  initializeIcons();
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  afterAll(() => {
+    nock.restore();
+  });
+
+  it('should successfully reset the password', async () => {
+    const scope = nock('http://localhost')
+      .post('/api/password-reset', {
+        email: 'person.one@example.com',
+      })
+      .reply(200);
+
+    render(
+      <Router initialEntries={['/password-reset']}>
+        <Route path="/password-reset" component={PasswordResetPage} />
+      </Router>
+    );
+
+    fireEvent.change(screen.getByLabelText('Email'), {
+      target: { value: 'person.one@example.com' },
+    });
+    fireEvent.click(screen.getByText('Absenden').closest('button'));
+
+    await waitFor(() =>
+      screen.getByText(
+        /Dein Passwort wurde erfolgreich zurück gesetzt. Bitte wende dich an/
+      )
+    );
+
+    expect(scope.isDone()).toBe(true);
+  });
+});
