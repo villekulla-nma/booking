fcf6639620a3f6f812761af102c060b9412f6942
Emanuel Kluge <mail@emanuel-kluge.de>
2021-03-28 19:04:37 +0200

    fix(client): hide logout button for anonymous users

--- client/src/components/logout-button.tsx
+++ client/src/components/logout-button.tsx
@@ -2,11 +2,17 @@ import type { FC } from 'react';
 import { PrimaryButton } from '@fluentui/react';
 
 import { logout } from '../api';
+import { useUser } from '../hooks/use-user';
 
 export const LogoutButton: FC = () => {
+  const user = useUser();
   const handleClick = () => {
     logout().then(() => window.location.reload());
   };
+
+  if (!user) {
+    return null;
+  }
 
   return <PrimaryButton onClick={handleClick}>Abmelden</PrimaryButton>;
 };
