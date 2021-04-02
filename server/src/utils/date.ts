f27e4b6ec96810edfe0d8b1d23334953376abd33
Emanuel Kluge <mail@emanuel-kluge.de>
2021-04-02 16:57:42 +0200

    fix(server): list all user events
    ...even those, that are all-day events and take place on the day of requesting the data

--- server/src/utils/date.ts
+++ server/src/utils/date.ts
@@ -1,14 +1,7 @@
-export const getNow = (): string => {
+export const getToday = (): string => {
   const date = new Date();
   const utc = new Date(
-    Date.UTC(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-      date.getHours(),
-      date.getMinutes(),
-      date.getSeconds()
-    )
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0, 0)
   );
 
   return utc.toISOString();
