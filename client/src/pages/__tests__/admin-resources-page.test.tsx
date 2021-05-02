6fb69869ffa1033e2d7bcebb0c514687dee31ad9
Emanuel Kluge <mail@emanuel-kluge.de>
2021-05-02 10:47:43 +0200

    test(client): check admin resources page

---
+++ client/src/pages/__tests__/admin-resources-page.test.tsx
@@ -0,0 +1,163 @@
+import type { FC } from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import nock from 'nock';
+import { initializeIcons } from '@uifabric/icons';
+
+import { scopeIsDone } from '../../helpers/nock';
+import { useUserContext } from '../../hooks/use-user-context';
+import { AdminResourcesPage } from '../admin-resources-page';
+import { inquireConfirmation } from '../../helpers/inquire-confirmation';
+
+jest.mock('../../hooks/use-user-context');
+jest.mock('../../helpers/inquire-confirmation');
+
+jest.mock('../../components/layout.tsx', () => {
+  const Layout: FC = ({ children }) => <>{children}</>;
+  return { Layout };
+});
+
+jest.mock('../../components/admin-layout.tsx', () => {
+  const AdminLayout: FC = ({ children }) => <>{children}</>;
+  return { AdminLayout };
+});
+
+describe('Admin Resources Page', () => {
+  const resources = [
+    {
+      id: 'Uj5SAS740',
+      name: 'Resource #1',
+    },
+    {
+      id: 'gWH5T7Kdz',
+      name: 'Resource #2',
+    },
+  ];
+  const newResource = {
+    id: 'Qij7dg39u',
+    name: 'Resource #3',
+  };
+  const user = {
+    id: 'TD0sIeaoz',
+    email: 'person.one@example.com',
+    firstName: 'Person1',
+    lastName: 'One',
+    fullName: 'Person1 One',
+    role: 'admin',
+  };
+
+  initializeIcons();
+
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
+
+  afterAll(() => {
+    nock.restore();
+  });
+
+  describe('basic behaviour', () => {
+    it('should render list of resources', async () => {
+      (useUserContext as jest.Mock).mockReturnValue(user);
+
+      const scope = nock('http://localhost')
+        .get('/api/resources')
+        .reply(200, { status: 'ok', payload: resources });
+
+      render(
+        <Router>
+          <AdminResourcesPage />
+        </Router>
+      );
+
+      await act(async () => {
+        await expect(scopeIsDone(scope)).resolves.toBe(true);
+      });
+
+      screen.getByText('Resource #1');
+      screen.getByText('Resource #2');
+    });
+  });
+
+  describe('adding a resource', () => {
+    it('should create a new resource', async () => {
+      (useUserContext as jest.Mock).mockReturnValue(user);
+
+      const initialScope = nock('http://localhost')
+        .get('/api/resources')
+        .reply(200, { status: 'ok', payload: resources });
+      const creationScope = nock('http://localhost')
+        .put('/api/resources', {
+          name: newResource.name,
+        })
+        .reply(200, { status: 'ok' })
+        .get('/api/resources')
+        .reply(200, { status: 'ok', payload: [...resources, newResource] });
+
+      render(
+        <Router>
+          <AdminResourcesPage />
+        </Router>
+      );
+
+      await act(async () => {
+        await expect(scopeIsDone(initialScope)).resolves.toBe(true);
+      });
+
+      expect(screen.queryByText('Resource #3')).toBeNull();
+
+      fireEvent.change(
+        screen.getByLabelText(/Create new resource/) as Element,
+        {
+          target: { value: newResource.name },
+        }
+      );
+
+      fireEvent.click(screen.getByText('Create').closest('button') as Element);
+
+      await act(async () => {
+        await expect(scopeIsDone(creationScope)).resolves.toBe(true);
+      });
+
+      screen.getByText('Resource #3');
+    });
+  });
+
+  describe('deleting a resource', () => {
+    it('should delete resource #2', async () => {
+      (useUserContext as jest.Mock).mockReturnValue(user);
+      (inquireConfirmation as jest.Mock).mockReturnValue(true);
+
+      const initialScope = nock('http://localhost')
+        .get('/api/resources')
+        .reply(200, { status: 'ok', payload: resources });
+      const deletionScope = nock('http://localhost')
+        .delete('/api/resources', {
+          id: resources[1].id,
+        })
+        .reply(200, { status: 'ok' })
+        .get('/api/resources')
+        .reply(200, { status: 'ok', payload: [resources[0]] });
+
+      render(
+        <Router>
+          <AdminResourcesPage />
+        </Router>
+      );
+
+      await act(async () => {
+        await expect(scopeIsDone(initialScope)).resolves.toBe(true);
+      });
+
+      screen.getByText('Resource #2');
+
+      fireEvent.click(screen.getByTestId(`delete-element-${resources[1].id}`));
+
+      await act(async () => {
+        await expect(scopeIsDone(deletionScope)).resolves.toBe(true);
+      });
+
+      expect(screen.queryByText('Resource #2')).toBeNull();
+    });
+  });
+});
