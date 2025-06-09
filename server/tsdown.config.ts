c6fa242556ec46b2d206dde79b0f9634bead003b
Emanuel Kluge <mail@emanuel-kluge.de>
2025-06-09 21:17:57 +0200

    chore(server): use `tsdown` for creating the prod build
    …and ditch `ncc`.

---
+++ server/tsdown.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsdown';
+import pkg from './package.json';
+
+export default defineConfig(({ watch }) => ({
+  entry: ['./src'],
+  outDir: '../dist/api',
+  target: ['node22'],
+  sourcemap: watch === true,
+  // NOTE: Bundling it all without giving up on the distinction
+  //       between dev- & prod dependencies. Except for `sqlite3`,
+  //       which can't be bundled because of its native bindings.
+  noExternal: Object.keys(pkg.dependencies).filter((mod) => mod !== 'sqlite3'),
+
+  // NOTE: Referenced by `sequelize`, but only relevant for postgres.
+  //       Setting this to be external to get rid of the warning in CI.
+  external: ['pg-hstore'],
+}));
